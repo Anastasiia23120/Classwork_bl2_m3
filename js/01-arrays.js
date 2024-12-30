1cc25281648ac31a76ebaf74bcc971a8f72d2596
Anastasiia Medved <nastya0662793393@gmail.com>
2024-12-30 01:45:33 +0100

    add part

---
+++ js/01-arrays.js
@@ -0,0 +1,19 @@
+const courses = ['HTML', 'CSS', 'JS'];
+
+console.log(courses);
+
+// console.log(courses[0]);
+// console.log(courses[1]);
+// console.log(courses[2]);
+// console.log(courses[10]);
+
+// courses[0] = 'React';
+// console.log(courses);
+
+// courses = 'Hello';
+
+const firstElement = courses[0];
+console.log(firstElement);
+
+const lastElement = courses[courses.length - 1];
+console.log(lastElement);
